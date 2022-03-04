5c8de88d4a7e7f0dcf1a587dd0f22278d86a9147
薛鹏 <100103081@qq.com>
2022-03-04 15:04:48 +0800

    [xuepeng]开发knife4j的SDK。

---
+++ shutter-sdk/shutter-sdk-knife4j/src/main/java/cn/org/niubility/shutter/sdk/knife4j/config/Knife4jConfiguration.java
@@ -0,0 +1,76 @@
+package cn.org.niubility.shutter.sdk.knife4j.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Knife4j的配置类。
+ *
+ * @author xuepeng
+ */
+@Configuration
+@EnableConfigurationProperties(Knife4jProperties.class)
+public class Knife4jConfiguration {
+
+
+    /**
+     * @return 创建API文档。
+     */
+    @Bean
+    public Docket createRestApi(final Knife4jProperties knife4jProperties) {
+        // 设置显示的swagger环境信息
+        Profiles profiles = Profiles.of(knife4jProperties.getProfiles().toArray(new String[0]));
+        // 判断是否处在自己设定的环境当中
+        boolean flag = environment.acceptsProfiles(profiles);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo(knife4jProperties))
+                .enable(flag)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(knife4jProperties.getBasePackage()))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * @return 创建API信息。
+     */
+    private ApiInfo apiInfo(final Knife4jProperties knife4jProperties) {
+        return new ApiInfoBuilder()
+                .description(knife4jProperties.getDescription())
+                .contact(new Contact(
+                        knife4jProperties.getAuthor(),
+                        knife4jProperties.getWebsite(),
+                        knife4jProperties.getEmail()))
+                .version(knife4jProperties.getVersion())
+                .title(knife4jProperties.getTitle())
+                .build();
+    }
+
+    /**
+     * 自动装配环境对象。
+     *
+     * @param environment 环境对象。
+     */
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    /**
+     * 环境对象。
+     */
+    private Environment environment;
+
+}
---
+++ shutter-sdk/shutter-sdk-knife4j/src/main/java/cn/org/niubility/shutter/sdk/knife4j/config/Knife4jProperties.java
@@ -0,0 +1,65 @@
+package cn.org.niubility.shutter.sdk.knife4j.config;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Knife4j自定义属性类。
+ *
+ * @author xuepeng
+ */
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "shutter.knife4j")
+public class Knife4jProperties {
+
+    /**
+     * 在以下环境下，会启动Knife4j。
+     */
+    private List<String> profiles = Arrays.asList("dev", "test");
+
+    /**
+     * API文档描述。
+     */
+    private String description;
+
+    /**
+     * API文档版本。
+     */
+    private String version;
+
+    /**
+     * API文档标题。
+     */
+    private String title;
+
+    /**
+     * 作者。
+     */
+    private String author;
+
+    /**
+     * 主页。
+     */
+    private String website;
+
+    /**
+     * 邮箱地址。
+     */
+    private String email;
+
+    /**
+     * 识别该包名下的Knife4j API。
+     */
+    private String basePackage = "cn.org.niubility.shutter";
+
+}
