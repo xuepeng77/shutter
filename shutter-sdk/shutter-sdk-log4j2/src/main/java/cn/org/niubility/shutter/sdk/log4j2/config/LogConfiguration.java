a0bf27738cfa9060f9488f5156dd266b51432d3c
薛鹏 <100103081@qq.com>
2022-03-11 10:34:47 +0800

    [xuepeng]开发Log4j2的SDK

--- shutter-sdk/shutter-sdk-log4j2/src/main/java/cn/org/niubility/shutter/sdk/log4j2/config/LogConfiguration.java
+++ shutter-sdk/shutter-sdk-log4j2/src/main/java/cn/org/niubility/shutter/sdk/log4j2/config/LogConfiguration.java
@@ -5,9 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Log4j2 SDK配置类。
+ * 注册日志拦截器。
+ *
+ * @author xuepeng
+ */
 @Configuration
-public class LoggerConfiguration implements WebMvcConfigurer {
+public class LogConfiguration implements WebMvcConfigurer {
 
+    /**
+     * 注册日志拦截器。
+     * 对所有API(/**)生效。
+     *
+     * @param registry 拦截器注册表。
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LogTrackInterceptor()).addPathPatterns("/**");
