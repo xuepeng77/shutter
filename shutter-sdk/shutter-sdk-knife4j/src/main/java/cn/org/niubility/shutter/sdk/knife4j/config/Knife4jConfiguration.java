864c6896e567dc02301d55f2b0f01d67038d2975
薛鹏 <100103081@qq.com>
2022-03-16 15:34:40 +0800

    [xuepeng]开启Knife4j日志增强。

--- shutter-sdk/shutter-sdk-knife4j/src/main/java/cn/org/niubility/shutter/sdk/knife4j/config/Knife4jConfiguration.java
+++ shutter-sdk/shutter-sdk-knife4j/src/main/java/cn/org/niubility/shutter/sdk/knife4j/config/Knife4jConfiguration.java
@@ -1,12 +1,15 @@
 package cn.org.niubility.shutter.sdk.knife4j.config;
 
 import cn.org.niubility.shutter.sdk.knife4j.property.Knife4jProperties;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,6 +17,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Knife4j的配置类。
@@ -21,6 +25,9 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @author xuepeng
  */
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
 @EnableConfigurationProperties(Knife4jProperties.class)
 public class Knife4jConfiguration {
 
