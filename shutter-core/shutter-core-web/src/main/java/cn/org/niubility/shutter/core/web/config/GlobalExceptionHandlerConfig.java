5d22713e5c5090eabf90f6b58c6f3001d262765a
薛鹏 <100103081@qq.com>
2022-03-26 02:11:30 +0800

    [xuepeng]设置参数校验异常。

---
+++ shutter-core/shutter-core-web/src/main/java/cn/org/niubility/shutter/core/web/config/GlobalExceptionHandlerConfig.java
@@ -0,0 +1,43 @@
+package cn.org.niubility.shutter.core.web.config;
+
+import cn.org.niubility.shutter.core.common.bean.api.DefaultResultFactory;
+import cn.org.niubility.shutter.core.common.bean.api.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 通用的异常处理类。
+ *
+ * @author xuepeng
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandlerConfig {
+
+    /**
+     * SpringBoot接口参数验证失败的异常处理。
+     *
+     * @param e MethodArgumentNotValidException。
+     * @return 错误信息。
+     */
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseBody
+    public Result<Map<String, String>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        log.error(e.getMessage());
+        final Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return DefaultResultFactory.param("接口参数验证失败。", errors);
+    }
+
+}
